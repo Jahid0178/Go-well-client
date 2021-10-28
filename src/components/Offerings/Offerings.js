44d0f2292252f12a2a8f7e8eab2d169ecb22f6e1
Jahid <jahidulhaque767@gmail.com>
2021-10-29 05:17:53 +0600

    modify image

--- src/components/Offerings/Offerings.js
+++ src/components/Offerings/Offerings.js
@@ -18,16 +18,19 @@ const Offerings = () => {
         <h1 className="text-center py-3 offer-title">Our Offer</h1>
         <Row className="g-3">
           {offers.map((offer) => (
-            <Col lg={4} key={offer._id}>
+            <Col lg={4} md={6} sm={12} key={offer._id}>
               <CardGroup>
                 <Card className="card-container mb-3">
                   <Card.Img variant="top" src={offer.img} />
-                  <Card.Body>
+                  <Card.Body className="c-body">
                     <Card.Title>{offer.name}</Card.Title>
                     <Card.Text>{offer.package}</Card.Text>
                     <Card.Text>{offer.description}</Card.Text>
-                    <Card.Text>{offer.places}</Card.Text>
-                    <p className="d-inline">Rating: {offer.rank}</p>
+                    <div className="d-flex justify-content-between">
+                      <Card.Text>{offer.places}</Card.Text>
+                      <p className="d-inline">Rating: {offer.rank}</p>
+                    </div>
+                    <p className="country-name">{offer.country}</p>
                   </Card.Body>
                   <Card.Footer className="d-flex justify-content-between">
                     <Button>Booking Now</Button>
