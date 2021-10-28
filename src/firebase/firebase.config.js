9e180700ede259c58397ee1367197351cfbdf7d3
Jahid <jahidulhaque767@gmail.com>
2021-10-29 03:14:46 +0600

    firebase config and init file

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDvTIYOz2RYv3k3bmMJ8k4mQDN5CYMrY6s",
+  authDomain: "go-well-travel.firebaseapp.com",
+  projectId: "go-well-travel",
+  storageBucket: "go-well-travel.appspot.com",
+  messagingSenderId: "796799303378",
+  appId: "1:796799303378:web:e8bcd54c9e367256b3b70b",
+};
+
+export default firebaseConfig;
