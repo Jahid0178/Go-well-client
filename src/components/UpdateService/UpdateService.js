a5903a5274fec54a476b29d2e6fd6dd089a9d044
Jahid <jahidulhaque767@gmail.com>
2021-10-30 02:59:10 +0600

    added update service

---
+++ src/components/UpdateService/UpdateService.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./UpdateService.css";
+
+const UpdateService = () => {
+  return (
+    <div>
+      <h2>This is update service</h2>
+    </div>
+  );
+};
+
+export default UpdateService;
