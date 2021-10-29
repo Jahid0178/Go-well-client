4a008e10d79564f715816cadd8546b133678da98
Jahid <jahidulhaque767@gmail.com>
2021-10-29 19:42:59 +0600

    complete login form

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,6 +1,7 @@
 import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { useForm } from "react-hook-form";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import useAuth from "../../hooks/useAuth";
 import login from "../../images/login.svg";
@@ -8,6 +9,8 @@ import "./LogIn.css";
 
 const LogIn = () => {
   const { signInUsingGoogle } = useAuth();
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
 
   const googleIcon = <FontAwesomeIcon icon={faGoogle} />;
   return (
@@ -15,13 +18,37 @@ const LogIn = () => {
       <Container>
         <div className="login-content">
           <Row className="d-flex align-items-center">
-            <Col>
-              <img src={login} style={{ width: 550 }} alt="" />
+            <Col lg={6}>
+              <img
+                className="img-fluid"
+                src={login}
+                style={{ width: 750 }}
+                alt=""
+              />
             </Col>
-            <Col>
-              <Button onClick={signInUsingGoogle}>
-                {googleIcon} Google Log In
-              </Button>
+            <Col lg={6}>
+              <div className="form-content">
+                <form
+                  onSubmit={handleSubmit(onSubmit)}
+                  className="form-container"
+                >
+                  <input
+                    type="email"
+                    placeholder="Enter your email"
+                    {...register("Email", { required: true, maxLength: 20 })}
+                  />
+                  <input
+                    placeholder="Enter your password"
+                    type="password"
+                    {...register("Password")}
+                  />
+
+                  <input type="submit" />
+                </form>
+                <Button className="mt-2" onClick={signInUsingGoogle}>
+                  {googleIcon} Google Log In
+                </Button>
+              </div>
             </Col>
           </Row>
         </div>
