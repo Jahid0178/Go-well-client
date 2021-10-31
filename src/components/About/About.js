fb3744b3b4d9592379a6cb2907fe9f27fbdf8716
Jahid <jahidulhaque767@gmail.com>
2021-10-31 20:20:14 +0600

    modify about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -16,16 +16,18 @@ const About = () => {
             <img className="img-fluid" src={aboutPerson} alt="" />
           </Col>
           <Col lg={8} md={6} sm={12}>
-            <h2>About US</h2>
-            <p>
-              There are many variations of passages of Lorem Ipsum available,
-              but the majority have suffered by injected humour, or randomised
-              words which don't look even slightly believable. If you are going
-              to use a passage of Lorem Ipsum It is a long established fact that
-              a reader will be distracted by the readable of a page when looking
-              at its layout. as opposed to using 'Content here, content here',
-              making it look like readable English.{" "}
-            </p>
+            <div className="mt-5">
+              <h2>About US</h2>
+              <p>
+                There are many variations of passages of Lorem Ipsum available,
+                but the majority have suffered by injected humour, or randomised
+                words which don't look even slightly believable. If you are
+                going to use a passage of Lorem Ipsum It is a long established
+                fact that a reader will be distracted by the readable of a page
+                when looking at its layout. as opposed to using 'Content here,
+                content here', making it look like readable English.{" "}
+              </p>
+            </div>
           </Col>
         </Row>
         <Works></Works>
